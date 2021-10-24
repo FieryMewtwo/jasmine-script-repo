4aaf8e3e45c8dbc84a300fa914c8c8aacad008d7
FieryMewtwo <77066742+FieryMewtwo@users.noreply.github.com>
2021-10-24 10:09:56 -0400

    Update and rename pokeemerald.sh to jasmine.sh

--- jasmine.sh
+++ jasmine.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 echo "Updating your system as a precaution, please wait..."
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get update && apt-get upgrade
 echo "Installing dependencies, please wait..."
 sudo apt install build-essential binutils-arm-none-eabi git libpng-dev
 echo "Cloning repositories, please wait..."
-git clone https://github.com/FieryMewtwo/pokeemerald
+git clone https://github.com/FieryMewtwo/jasmine
 git clone https://github.com/pret/agbcc
 cd agbcc
-echo "Compiling pokeemerald.gba, please wait..."
+echo "Compiling jasmine from source, please wait..."
 ./build.sh
-./install.sh ../pokeemerald
+./install.sh ../jasmine
 cd ..
-cd pokeemerald
-make compare
+cd jasmine
 make
-echo "Done! You should now have the rom in the pokeemerald directory, ready to release to git.
+echo "Done! You should now have the rom in the jasmine directory.
